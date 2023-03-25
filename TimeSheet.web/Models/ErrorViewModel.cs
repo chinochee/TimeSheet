2e35e70232b010984f387e29e9d9ff38b97735fc
Никита Ефремов <n.efremov@life-pay.ru>
2023-03-25 17:11:15 +0300

    Initial commit

---
+++ TimeSheet.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TimeSheet.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
