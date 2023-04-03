42c9ec7d00190ae4c3c0070da10fbfc82c27abff
Никита Ефремов <n.efremov@life-pay.ru>
2023-04-03 22:38:03 +0300

    Господь спас

---
+++ TimeSheet.Web/Models/TimeSheetTableModel.cs
@@ -0,0 +1,10 @@
+﻿using Services.Dtos;
+
+namespace TimeSheet.Web.Models
+{
+    public class TimeSheetTableModel
+    {
+        public TimeSheetEntryDto[] Entries { get; set; }
+        public TimeSheetFiltersDto Filters { get; set; }
+    }
+}
