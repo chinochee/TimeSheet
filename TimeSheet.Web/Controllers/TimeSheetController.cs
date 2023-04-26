730376bfa4212b44af21e800b10c1070fed5f058
Никита Ефремов <n.efremov@life-pay.ru>
2023-04-26 23:10:23 +0300

    Внес мелкую правку

--- TimeSheet.Web/Controllers/TimeSheetController.cs
+++ TimeSheet.Web/Controllers/TimeSheetController.cs
@@ -6,9 +6,9 @@ namespace TimeSheet.Web.Controllers
 {
     public class TimeSheetController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<TimeSheetController> _logger;
         private readonly ITimeSheetTableService _timeSheetTableService;
-        public TimeSheetController(ILogger<HomeController> logger, ITimeSheetTableService timeSheetTableService)
+        public TimeSheetController(ILogger<TimeSheetController> logger, ITimeSheetTableService timeSheetTableService)
         {
             _logger = logger;
             _timeSheetTableService = timeSheetTableService;
