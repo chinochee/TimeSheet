e34842f6ccebb26f251825dc63c2e2c5c9f41aec
Никита Ефремов <n.efremov@life-pay.ru>
2023-05-07 18:15:37 +0300

    Добавил выгрузку валют в табличном представлении курса валют

--- TimeSheet.Web/Controllers/CurrencyController.cs
+++ TimeSheet.Web/Controllers/CurrencyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Services;
 
 namespace TimeSheet.Web.Controllers
@@ -19,6 +20,31 @@ namespace TimeSheet.Web.Controllers
         {
             var currencies = await _currencyService.Get();
             return View(currencies);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OnGetCurrencies()
+        {
+            var currencies = await _currencyService.Get();
+
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Exchange rates");
+            worksheet.Cell("A1").Value = "Full name";
+            worksheet.Cell("B1").Value = "Short name";
+            worksheet.Cell("C1").Value = "Dollar exchange rate";
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                for (var i = 0; i < currencies.Length; i++)
+                {
+                    worksheet.Cell(i + 2, 1).Value = currencies[i].FullName;
+                    worksheet.Cell(i + 2, 2).Value = currencies[i].ShortName;
+                    worksheet.Cell(i + 2, 3).Value = currencies[i].DollarExchangeRate;
+                }
+                workbook.SaveAs(stream);
+
+                return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ExchangeRates.xlsx");
+            }
         }
     }
 }
