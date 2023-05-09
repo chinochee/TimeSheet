5875ee89ebd38944793f10c84bea167fe67d7ed8
Никита Ефремов <n.efremov@life-pay.ru>
2023-05-09 21:56:33 +0300

    Теперь при изменении объекта используется метод update контекста, а не удаление с последующим добавлением измененной сущности

--- Services/CurrencyService.cs
+++ Services/CurrencyService.cs
@@ -24,26 +24,22 @@ namespace Services
 
         public void Post(Currency[] currencyEntryDto)
         {
-            var addCurrencyList = new List<Currency>();
             foreach (var currency in currencyEntryDto)
             {
                 if (_context.Currencies.Any(c => c.FullName == currency.FullName))
                 {
                     var editCurrency = _context.Currencies.FirstOrDefault(c => c.FullName == currency.FullName);
-                    _context.Currencies.Remove(editCurrency);
 
                     editCurrency.ShortName = currency.ShortName;
                     editCurrency.DollarExchangeRate = currency.DollarExchangeRate;
-                    addCurrencyList.Add(editCurrency);
+                    _context.Currencies.Update(editCurrency);
                 }
                 else
                 {
-                    addCurrencyList.Add(currency);
+                    _context.Currencies.Add(currency);
                 }
             }
 
-            _context.SaveChanges();
-            _context.Currencies.AddRange(addCurrencyList.ToArray());
             _context.SaveChanges();
         }
     }
