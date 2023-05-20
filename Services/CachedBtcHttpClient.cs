a00f58e530a478b6f1f44ed77cb92e1114dc6bec
Никита Ефремов <n.efremov@life-pay.ru>
2023-05-20 22:55:58 +0300

    Реализовал лимит запросов к получению курса btc

--- Services/CachedBtcHttpClient.cs
+++ Services/CachedBtcHttpClient.cs
@@ -9,6 +9,8 @@ namespace Services
         private readonly IBitcoinHttpClient _component;
         private readonly IMemoryCache _memoryCache;
         private readonly CacheSettings _cacheSettings;
+        private static SemaphoreSlim _semaphore = new(1, 1);
+        private const string _key = "rate";
 
         public CachedBtcHttpClient(IBitcoinHttpClient component, CacheSettings config, IMemoryCache memoryCache)
         {
@@ -19,14 +21,22 @@ namespace Services
 
         public async Task<RatesDto> GetRates()
         {
-            if (!_memoryCache.TryGetValue("rate", out RatesDto cacheValue))
-            {
-                var rates = await _component.GetRates();
-                _memoryCache.Set("rate", rates, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(_cacheSettings.SecondsHoldCache)));
-                return rates;
-            }
+            if (_memoryCache.TryGetValue(_key, out RatesDto cacheValue)) return cacheValue;
 
-            return cacheValue;
+            await _semaphore.WaitAsync();
+
+            try
+            {
+                if (_memoryCache.TryGetValue(_key, out cacheValue)) return cacheValue;
+
+                cacheValue = await _component.GetRates();
+                _memoryCache.Set(_key, cacheValue, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromSeconds(_cacheSettings.SecondsHoldCache)));
+                return cacheValue;
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
         }
     }
 }
