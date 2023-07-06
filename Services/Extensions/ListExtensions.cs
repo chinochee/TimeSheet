fb1b4bbc25dfcceb34deba25e492ba7d56840648
Никита Ефремов <n.efremov@life-pay.ru>
2023-07-06 21:23:54 +0300

    Добавил экстеншен для листа с методом RemoveDuplicates, который убирает дубли из листов

---
+++ Services/Extensions/ListExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Services.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<T> RemoveDuplicates<T>(List<T> list)
+        {
+            return new HashSet<T>(list).ToList();
+        }
+    }
+}
